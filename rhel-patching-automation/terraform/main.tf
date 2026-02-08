fc8278e7139429ec5e4a364ac6438bdf5cfddf62
Luis Francisco <lrfrancisco17@gemail@example.com>
2026-02-08 08:58:03 -0500

    adding  availability_zone = us-east-1a to main.tf

--- rhel-patching-automation/terraform/main.tf
+++ rhel-patching-automation/terraform/main.tf
@@ -24,6 +24,7 @@ resource "aws_subnet" "lab" {
   vpc_id                  = aws_vpc.lab.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
 
   tags = {
     Name = "lab-subnet"
