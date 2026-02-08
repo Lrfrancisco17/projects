3edb4012f68d6dbab363996e29e0bb57cd8f5623
Luis Francisco <lrfrancisco17@gemail@example.com>
2026-02-08 07:34:00 -0500

    creating ansible workflow & adding changes to terraform yml files

--- rhel-patching-automation/terraform/modules/ec2-instance/main.tf
+++ rhel-patching-automation/terraform/modules/ec2-instance/main.tf
@@ -4,7 +4,9 @@ resource "aws_instance" "this" {
   subnet_id              = var.subnet_id
   vpc_security_group_ids = var.security_groups
 
-  user_data = file(var.cloud_init)
+  user_data = templatefile(var.cloud_init, {
+    ssh_pubkey = file(var.ssh_pubkey_path)
+  })
 
   tags = var.tags
 }
--- rhel-patching-automation/terraform/modules/ec2-instance/variables.tf
+++ rhel-patching-automation/terraform/modules/ec2-instance/variables.tf
@@ -3,5 +3,6 @@ variable "instance_type" { type = string }
 variable "subnet_id" { type = string }
 variable "security_groups" { type = list(string) }
 variable "cloud_init" { type = string }
+variable "ssh_pubkey_path" { type = string }
 variable "tags" { type = map(string) }
 
